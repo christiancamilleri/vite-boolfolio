ffc36ce3290355753eb49992f798cb18744d655a
christiancamilleri <camilleri.christian@outlook.com>
2023-05-29 14:53:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+
+
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <AppMain></AppMain>
+  </div>
+</template>
+
+<style ></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script >
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  components: {
+
+  },
+  created() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(res => {
+        // console.log(res.data.results);
+        this.projects = res.data.results;
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  prova
+</template>
+
+<style></style>
